358a728b8c49475158ccca859760c15068c1e6bc
D4n3x <76569311+Pwn2Ninj4@users.noreply.github.com>
2021-04-10 21:25:03 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+Uiii
 #!/usr/bin/bash
 
 #Colors
@@ -6,6 +7,7 @@ red="\e[0;31m\033[1m"
 end="\033[0m"
 
 echo -e "\n${red}[!]${end}${purple}Compile the code with gcc...${end}"
+
 gcc -o nex-dbg nex-dbg.c &> /dev/null
 
 cp -r nex-dbg /usr/bin/
