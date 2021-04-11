aff941f1e1df77b866c10abf82af1012f64fad92
D4n3x <76569311+Pwn2Ninj4@users.noreply.github.com>
2021-04-10 21:24:36 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,4 +8,6 @@ end="\033[0m"
 echo -e "\n${red}[!]${end}${purple}Compile the code with gcc...${end}"
 gcc -o nex-dbg nex-dbg.c &> /dev/null
 
-cp -r nex-dbg /usr/bin
+cp -r nex-dbg /usr/bin/
+
+echo -e "\n${red}[+]${end}${purple}Done :)${end}"
